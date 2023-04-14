33e907088d6f78691ab1bf60f0189c1f5c9be977
Kanishk <kanishksinghal06@gmail.com>
2023-04-14 23:13:53 +0530

    Added code for starting position

--- src/com/bl/snakeandladder/SnakeAndLadder.java
+++ src/com/bl/snakeandladder/SnakeAndLadder.java
@@ -2,6 +2,6 @@ package com.bl.snakeandladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game");
-
+        int position = 0;
     }
 }
