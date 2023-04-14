2058ec59e2f2b8a722e1942f301be2a5e60f5c15
Kanishk <kanishksinghal06@gmail.com>
2023-04-14 23:39:00 +0530

    Added the code for checking options

--- src/com/bl/snakeandladder/SnakeAndLadder.java
+++ src/com/bl/snakeandladder/SnakeAndLadder.java
@@ -4,6 +4,19 @@ public class SnakeAndLadder {
         System.out.println("Welcome to the Snake and Ladder Game");
         int position = 0;
         int dieroll = (int)((Math.random() * 6) + 1);
+        int options = (int)(Math.random() * 3);
+        switch (options) {
+            case 1 : // Case for Ladder
+                position += dieroll;
+                System.out.println("Got a Ladder move ahead by: "+position);
+                break;
+            case 2 : // Case for Snake
+                position -= dieroll;
+                System.out.println("Got a Snake come back by: "+position);
+                break;
+            default: // Case of No Play
+                System.out.println("No Play!!!");
+        }
 
     }
 }
