34fbb875620e3d0315fa52a4cf2c7471e2a326da
Kanishk <kanishksinghal06@gmail.com>
2023-04-15 00:59:35 +0530

    Added the code for player reaches the winning position 100

--- src/com/bl/snakeandladder/SnakeAndLadder.java
+++ src/com/bl/snakeandladder/SnakeAndLadder.java
@@ -3,20 +3,28 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game");
         int position = 0;
-        int dieroll = (int)((Math.random() * 6) + 1);
-        int options = (int)(Math.random() * 3);
-        switch (options) {
-            case 1 : // Case for Ladder
-                position += dieroll;
-                System.out.println("Got a Ladder move ahead by: "+position);
-                break;
-            case 2 : // Case for Snake
+        int WinningPosition = 100;
+        while (position < WinningPosition) {
+            int dieroll = (int) ((Math.random() * 6) + 1);
+            int options = (int) (Math.random() * 3);
+            switch (options) {
+                case 1: // Case for Ladder
+                    position += dieroll;
+                    System.out.println("Got a Ladder move ahead by: " + position);
+                    break;
+                case 2: // Case for Snake
+                    position -= dieroll;
+                    System.out.println("Got a Snake come back by: " + position);
+                    break;
+                default: // Case of No Play
+                    System.out.println("No Play!!!");
+            }
+            if (position < 0) {
+                position = 0;
+            } else if (position > WinningPosition) {
                 position -= dieroll;
-                System.out.println("Got a Snake come back by: "+position);
-                break;
-            default: // Case of No Play
-                System.out.println("No Play!!!");
+            }
         }
-
+        System.out.println("Player at Winning Position: " + position);
     }
 }
